c99eaf6aadcf20e33765e7a550eed4c61e0cc414
lbalbrecht <lalbrecht18@wooster.edu>
2021-03-30 23:23:36 -0700

    final commit before submission

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # lba-code-quiz-homework
+
+## User Story
+This app will allow the user to brush up on their knowledge with a short, multiple choice quiz.
+
+## Usage
+The user will start the quiz by pressing the "Begin" button. for each question, if the user selects the correct answer, their score will go up and the app will display "Correct" below the questions; if the user selects the incorrect answer, the time left will decrease and the app will display "Incorrect." The user's final score will be their number of correct answers plus the remaining time left. The user can choose to play again if desired to improve their score
+
+In the future, users will be able to log their scores onto a high score table and view the table by clicking the "View High Scores" button on top of the page.
+
+![Deployed on Github Pages] (https://lbalbrecht.github.io/lba-code-quiz-homework/)
+
+![Github repository] (https://github.com/lbalbrecht/lba-code-quiz-homework)
+
+<img src="assets\images\code-quiz-snapshot.png" alt = "code quiz landing page snapshot">
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,18 @@
 var timerEl = document.querySelector("#timer");
+var contentEl = document.querySelector(".content");
 var titleEl = document.querySelector("#title");
 var textFieldEl = document.querySelector("#text-field");
-var optionsEl = document.querySelector("#options")
+var answerListEl = document.querySelector("#options")
 var beginEl = document.querySelector("#begin");
 var resultEl = document.querySelector("#result");
+var highScoreEl = document.createElement("button")
+var submitScoreEl = document.createElement("button")
 
-var secondsLeft = 11;
+var secondsLeft = 10;
 var playerScore = 0;
 
-var answerListEl = document.createElement("ol")
-
-var li1 = document.createElement("li")
-var li2 = document.createElement("li")
-var li3 = document.createElement("li")
-var li4 = document.createElement("li")
-
-var choice1 = document.createElement("button")
-var choice2 = document.createElement("button")
-var choice3 = document.createElement("button")
-var choice4 = document.createElement("button")
-
-li1.appendChild(choice1)
-li2.appendChild(choice2)
-li3.appendChild(choice3)
-li4.appendChild(choice4)
-
-answerListEl.appendChild(li1)
-answerListEl.appendChild(li2)
-answerListEl.appendChild(li3)
-answerListEl.appendChild(li4)
-
 function timerStart() {
-    var interval = setInterval(function () {
+    interval = setInterval(function () {
         secondsLeft--;
         timerEl.textContent = "Time left: " + secondsLeft;
 
@@ -40,168 +21,117 @@ function timerStart() {
             alert("Time's up!")
             showScore();
         }
+
     }, 1000)
 
 }
 
-var question1 = {
+var questionsArray = [{
     question: "What color is the sky?",
     answer1: "green",
-    // correct:
     answer2: "blue",
     answer3: "yellow",
     answer4: "pink",
-}
+    correct: "blue",
+},
 
-var question2 = {
+{
     question: "Which of the following is not a state capital?",
     answer1: "Columbus",
     answer2: "Olympia",
-    // correct:
     answer3: "Los Angeles",
     answer4: "Tallahassee",
-}
+    correct: "Los Angeles",
+},
 
-var question3 = {
+{
     question: "What is the population of Seattle (approximate, in thousands?",
-    // correct:
     answer1: "775",
     answer2: "700",
     answer3: "800",
     answer4: "825",
-}
+    correct: "775",
+},
 
-var question4 = {
+{
     question: "Do you believe in a thing called love?",
     answer1: "yes",
     answer2: "no",
     answer3: "I don't know",
-    // correct:
     answer4: "We'll be rockin' 'til the sun goes down",
-}
+    correct: "We'll be rockin' 'til the sun goes down",
+}]
+
+var currentIndex = 0
+var choice1 = document.querySelector("#choice1")
+var choice2 = document.querySelector("#choice2")
+var choice3 = document.querySelector("#choice3")
+var choice4 = document.querySelector("#choice4")
+var answerBtn = document.querySelectorAll(".btn")
 
 beginEl.addEventListener("click", function () {
     timerStart();
-    beginEl.setAttribute("visibility", "hidden");
-    questionOne();
+    playerScore = 0;
+    secondsLeft = 10;
+    currentIndex = 0;
+    timerEl.setAttribute("style", "visibility:visible");
+    answerListEl.setAttribute("style", "visibility:visible");
+    beginEl.setAttribute("style", "display:none");
+    submitScoreEl.setAttribute("style", "visibility:hidden");
+    highScoreEl.setAttribute("style", "visibility:hidden");
+    startQuiz();
 })
 
-function questionOne() {
-    textFieldEl.textContent = question1.question;
-    optionsEl.appendChild(answerListEl);
-    choice1.textContent = question1.answer1;
-    choice2.textContent = question1.answer2;
-    choice3.textContent = question1.answer3;
-    choice4.textContent = question1.answer4;
-
-    choice1.addEventListener("click", function () {
-        secondsLeft-2;
-        questionTwo();
-    })
-    choice2.addEventListener("click", function () {
-        playerScore++;
-        questionTwo();
-        console.log(playerScore);
-    })
-    choice3.addEventListener("click", function () {
-        secondsLeft-2;
-        questionTwo();
-    })
-    choice4.addEventListener("click", function () {
-        secondsLeft-2;
-        questionTwo();
-    })
+function startQuiz() {
+    textFieldEl.textContent = questionsArray[currentIndex].question;
+    choice1.textContent = questionsArray[currentIndex].answer1;
+    choice2.textContent = questionsArray[currentIndex].answer2;
+    choice3.textContent = questionsArray[currentIndex].answer3;
+    choice4.textContent = questionsArray[currentIndex].answer4;
 
 }
 
-function questionTwo() {
-    textFieldEl.textContent = question2.question;
-    choice1.textContent = question2.answer1;
-    choice2.textContent = question2.answer2;
-    choice3.textContent = question2.answer3;
-    choice4.textContent = question2.answer4;
+answerBtn.forEach(function (ansBtn){
+    ansBtn.addEventListener("click", function(event) {
+        var userGuess = event.target.textContent;
+        if (userGuess === questionsArray[currentIndex].correct) {
+            playerScore++;
+            currentIndex++;
+            resultEl.textContent = "Correct!"
+            resultEl.setAttribute("style", "visibility:visible")
+            console.log(playerScore)
+        }
+        else {
+            currentIndex++;
+            secondsLeft -= 2;
+            resultEl.textContent = "Incorrect!"
+            resultEl.setAttribute("style", "visibility:visible")
+        }
 
-    choice1.addEventListener("click", function () {
-        secondsLeft-2;
-        questionThree();
+        if (currentIndex >= questionsArray.length) {
+            showScore();
+        }
+        startQuiz();
     })
-    choice2.addEventListener("click", function () {
-        secondsLeft-2;
-        questionThree();
-    })
-    choice3.addEventListener("click", function () {
-        playerScore++;
-        console.log(playerScore);
-        questionThree();
-    })
-    choice4.addEventListener("click", function () {
-        secondsLeft-2;
-        questionThree();
-    })
-
-}
-
-function questionThree() {
-    textFieldEl.textContent = question3.question;
-    choice1.textContent = question3.answer1;
-    choice2.textContent = question3.answer2;
-    choice3.textContent = question3.answer3;
-    choice4.textContent = question3.answer4;
-
-    choice1.addEventListener("click", function () {
-        playerScore++;
-        console.log(playerScore);
-        questionFour();
-    })
-    choice2.addEventListener("click", function () {
-        secondsLeft-2;
-        questionFour();
-    })
-    choice3.addEventListener("click", function () {
-        secondsLeft-2;
-        questionFour();
-    })
-    choice4.addEventListener("click", function () {
-        secondsLeft-2;
-        questionFour();
-    })
-
-}
-
-function questionFour() {
-    textFieldEl.textContent = question4.question;
-    choice1.textContent = question4.answer1;
-    choice2.textContent = question4.answer2;
-    choice3.textContent = question4.answer3;
-    choice4.textContent = question4.answer4;
-
-    choice1.addEventListener("click", function () {
-        secondsLeft-2;
-        showScore();
-    })
-    choice2.addEventListener("click", function () {
-        secondsLeft-2;
-        showScore();
-    })
-    choice3.addEventListener("click", function () {
-        secondsLeft-2;
-        showScore();
-    })
-    choice4.addEventListener("click", function () {
-        playerScore++;
-        console.log(playerScore);
-        showScore();
-    })
-
-    
-}
+})
 
 function showScore() {
     titleEl.textContent = "Final Score";
-    textFieldEl.textContent = playerScore;
-    optionsEl.remove();
-    document.appendChild(beginEl)
+    textFieldEl.textContent = playerScore + " points, " + secondsLeft + " seconds left";
+    contentEl.appendChild(beginEl)
+    beginEl.setAttribute("style", "visibility:visible");
     beginEl.textContent = "Play again"
+    contentEl.appendChild(submitScoreEl)
+    submitScoreEl.setAttribute("style", "visibility:visible");
+    submitScoreEl.textContent = "Submit score"
+    contentEl.appendChild(highScoreEl)
+    highScoreEl.setAttribute("style", "visibility:visible");
+    highScoreEl.textContent = "View high Scores"
     timerEl.remove();
+    answerListEl.setAttribute("style", "display:none");
+    clearInterval(interval)
 }
 
+// submitScoreEl.addEventListener("click", function(event) {
+
+// })
